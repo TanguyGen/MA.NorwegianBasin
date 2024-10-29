879f77e1dd387931744788abeb612e85be7fa9f2
Tanguy Genthon <72631088+TanguyGen@users.noreply.github.com>
2024-10-29 10:05:04 +0100

    Change map

--- Implementation doc/R scripts/map_habitat.R
+++ Implementation doc/R scripts/map_habitat.R
@@ -1,12 +1,12 @@
 
-packages <- c("tidyverse", "data.table", "ncdf4", "stars", "rayshader", "tictoc","ggmap","ggOceanMaps","sf")   # List packages
+packages <- c("ggOceanMaps","sf","dplyr")   # List packages
 lapply(packages, library, character.only = TRUE)    
 
 habitat<-readRDS("./Implementation doc/R scripts/Data/Habitats.rds") 
 
 Inshore_colors <- c(
   "Gravel" = "#f2f0f7",
-  "Rock" = "#ffedbd",  
+  "Rock" = "grey",  
   "Sand" = "#ffd25f", 
   "Silt" = "#ffb700"
 )
@@ -16,6 +16,10 @@ inshore_data <- subset(habitat, Shore == "Inshore")
 offshore_data <- subset(habitat, Shore == "Offshore")
 
 dt <- data.frame(lon = c(-5, -5, 15, 15), lat = c(60, 75, 60, 75))
+ICES<-ices_areas%>%
+  filter(Area_Full=="27.2.a.1"|Area_Full=="27.2.a.2")%>%
+  st_transform(crs=st_crs(inshore_data))
+
 
 basemap(data = dt,land.col="black")+
   geom_sf(data = inshore_data, aes(fill = Habitat), color = "NA", size = 0.2) +
@@ -23,6 +27,24 @@ basemap(data = dt,land.col="black")+
   ggnewscale::new_scale_fill() +
   geom_sf(data = offshore_data, aes(fill = Habitat), color = "NA", size = 0.2) +
   scale_fill_manual(values = Offshore_colors, name = "Offshore Habitats", guide = guide_legend(order = 2))+
+  ggnewscale::new_scale_fill() +
+  geom_sf(data = ICES, aes(color = "red"),linewidth = 0.6,alpha=0) +
+  scale_color_manual(values="red",name = "ICES areas 27.2", labels="",guide = guide_legend(order = 3))+
   theme_minimal() +
   coord_sf()+
-  labs(fill = "Zone and Habitat")
+  labs(fill = NULL)
+
+ggsave("./Implementation doc/img/habitats.png", dpi = 1500)
+
+
+domain <- readRDS("./Objects/Domains.rds") 
+
+
+basemap(data = dt,land.col="black",bathymetry =TRUE) +
+  geom_sf(data = domain, alpha=0,linewidth = 0.6, color = "red", size = 0.2) +
+  theme_minimal() +
+  coord_sf()+
+  labs(fill = NULL)
+
+
+
