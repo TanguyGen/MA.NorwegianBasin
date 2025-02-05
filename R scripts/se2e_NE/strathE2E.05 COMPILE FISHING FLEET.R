a7c0f53c89adf1614fc81d5ed621e3361b2accc8
Jack Laverick <jack.laverick@strath.ac.uk>
2025-02-05 20:43:16 +0000

    bug: correct bug in reordering habitat efforts, and commit to messy filepaths because nothing else seems possible.

--- R scripts/se2e_NE/strathE2E.05 COMPILE FISHING FLEET.R
+++ R scripts/se2e_NE/strathE2E.05 COMPILE FISHING FLEET.R
@@ -1,77 +1,86 @@
 
 #### Setup                                            ####
-
+rm(list=ls())
 library(tidyverse)
 library(stringr)
 source("./R scripts/@_Region file.R")
 
-discard_rate <- readRDS("./Data/Norwegian_sea_Tanguy2/Norwegian_sea/2010-2019/Object/EU discard rates.rds") %>%   # Import data
-.[,-1]
 
-landings_raw <- readRDS("./Data/Norwegian_sea_Tanguy2/Norwegian_sea/2010-2019/Object/International landings.rds")  # Units tonnes/m2/year
 
-effort <- t(readRDS("./Data/Norwegian_sea_Tanguy2/Norwegian_sea/2010-2019/Object/International effort by gear.rds"))        # Units sec/m2/day
+discard_rate <- readRDS("./Data/Fishing_objects_01_25_TG/Data/Norwegian_sea_Tanguy2/Norwegian_sea/2010-2019/Object/Discard rates.rds")  # Import data
 
-distribution <- t(readRDS("./Data/Norwegian_sea_Tanguy2/Norwegian_sea/2010-2019/Object/International effort proportion by gear and habitat.rds"))
+bycatch<-readRDS("./Data/Fishing_objects_01_25_TG/Data/Norwegian_sea_Tanguy2/Norwegian_sea/2010-2019/Object/Bycatch weight.rds")
 
-lookup <- read.csv("./Data/lookup_gear.csv") %>% select(-X) %>% arrange(neworder)  # Import and order tables according to StrathE2E
-hablookup <- read.csv("./Data/lookup_habitat.csv") %>% select(-X) %>% arrange(hneworder)
-glookup <- read.csv("./Data/lookup_guild.csv") %>% select(-X) %>% arrange(gneworder)
+landings_raw <- readRDS("./Data/Fishing_objects_01_25_TG/Data/Norwegian_sea_Tanguy2/Norwegian_sea/2010-2019/Object/International landings.rds")  # Units tonnes/m2/year
 
-domain_size <- readRDS("./Objects/Domains.rds") %>%                         # We need landings as tonnes per m^2
-  sf::st_union() %>% 
-  sf::st_area() %>% 
-  as.numeric()
+effort <- t(readRDS("./Data/Fishing_objects_01_25_TG/Data/Norwegian_sea_Tanguy2/Norwegian_sea/2010-2019/Object/International effort by gear.rds"))        # Units sec/m2/day
+
+distribution <- t(readRDS("./Data/Fishing_objects_01_25_TG/Data/Norwegian_sea_Tanguy2/Norwegian_sea/2010-2019/Object/International effort proportion by gear and habitat.rds"))
+
+
+lookup <- read.csv("./Data/Fishing_objects_01_25_TG/Data/lookup_gear.csv") %>% select(-X) %>% arrange(neworder)  # Import and order tables according to StrathE2E
+hablookup <- read.csv("./Data/Fishing_objects_01_25_TG/Data/lookup_habitat.csv") %>% select(-X) %>% arrange(hneworder)
+glookup <- read.csv("./Data/Fishing_objects_01_25_TG/Data/lookup_guild.csv") %>% select(-X) %>% arrange(gneworder)
+
 
 #### quickly copy over Tanguy's fish processing and fleet files ####
 
-processing <- file.copy("./Data/Norwegian_sea_Tanguy2/Norwegian_sea/2010-2019/Param/fishing_processing_NORWEGIAN_BASIN_2010-2019.csv",
-                        str_glue("./StrathE2E/{implementation}/2010-2019-{ssp}/Param/fishing_processing_NORWEGIAN_BASIN_2010-2019.csv"))
+processing <- file.copy("./Data/Norwegian_Basin_10_24_TG/Norwegian_Basin/2010-2019-ssp370/Param/fishing_processing_NORWEGIAN_BASIN_2010-2019.csv",
+                        str_glue("./StrathE2E/{implementation}/2010-2019/Param/fishing_processing_NORWEGIAN_BASIN_2010-2019.csv"))
 
-fleet <-file.copy("./Data/Norwegian_sea_Tanguy2/Norwegian_sea/2010-2019/Param/fishing_fleet_NORWEGIAN_BASIN_2010-2019.csv",
-                  str_glue("./StrathE2E/{implementation}/2010-2019-{ssp}/Param/fishing_fleet_NORWEGIAN_BASIN_2010-2019.csv"))
+fleet <-file.copy("./Data/Norwegian_Basin_10_24_TG/Norwegian_Basin/2010-2019-ssp370/Param/fishing_fleet_NORWEGIAN_BASIN_2010-2019.csv",
+                  str_glue("./StrathE2E/{implementation}/2010-2019/Param/fishing_fleet_NORWEGIAN_BASIN_2010-2019.csv"))
 
-mults <- read.csv("./Data/Norwegian_sea_Tanguy2/Norwegian_sea/2010-2019/Param/fishing_gear_multiplier.csv", sep = ";") %>% 
+mults <- read.csv("./Data/Norwegian_Basin_10_24_TG/Norwegian_Basin/2010-2019-ssp370/Param/fishing_gear_multiplier_NORWEGIAN_BASIN_2010-2019.csv", sep = ",") %>% 
   select(-Gear_code) %>% 
   full_join(rename(lookup, Gear_name = newgears, Gear_code = gearcodes)) %>% 
   drop_na(rawgears) %>% 
   select(Gear_name, Gear_code, starts_with("Multi")) %>% 
   mutate(Multiplier_to_be_applied_to_activity = 1) %>% 
-  write.csv(str_glue("./StrathE2E/{implementation}/2010-2019-{ssp}/Param/fishing_gear_multiplier_NORWEGIAN_BASIN_2010-2019.csv"),row.names=FALSE)
-  
+  write.csv(str_glue("./StrathE2E/{implementation}/2010-2019/Param/fishing_gear_multiplier_{toupper(implementation)}_2010-2019.csv"),row.names=FALSE)
+
 #### Insert missing factor levels ####
 
 blank_fleetxguild <- matrix(0, 12, 12)
 
-colnames(blank_fleetxguild) <- c(colnames(effort), paste0("blank", 1:4))
+colnames(blank_fleetxguild) <- c(colnames(effort), paste0("blank", 1))
 rownames(blank_fleetxguild) <- colnames(landings_raw)
 
-blank_fleetxguild[1, 1:8] <- effort[1,]
+blank_fleetxguild[1, 1:11] <- effort[1,]
 effort <- blank_fleetxguild[1, ] 
+
 
 blank_fleetxguild[] <- 0
 blank_fleetxguild <- t(blank_fleetxguild)
-blank_fleetxguild[1:8, 1:12] <- discard_rate
+blank_fleetxguild[1:11, 1:12] <- discard_rate
 
 discard_rate <- blank_fleetxguild
 
+
 blank_fleetxguild[] <- 0
-blank_fleetxguild[1:8, 1:12] <- landings_raw
+blank_fleetxguild[1:12, 1:11] <- bycatch
+
+bycatch <- blank_fleetxguild
+
+blank_fleetxguild[] <- 0
+blank_fleetxguild[1:11, 1:12] <- landings_raw
 
 landings_raw <- blank_fleetxguild
 
 
-missing_habs <- distribution[,1:4]
+missing_habs <- distribution[,2]
 missing_habs[] <-0
-colnames(missing_habs) <- str_replace(colnames(missing_habs), "In", "Off")  
 
 distribution <- cbind(distribution, missing_habs)
 
-missing_gears <- distribution[1:4,]
-missing_gears[] <-0
-rownames(missing_gears) <- paste0("blank", 1:4)  
+colnames(distribution)[ncol(distribution)] <- "Offshore Rock"  
 
+missing_gears <- distribution[1,]
+missing_gears[] <-0
 distribution <- rbind(distribution, missing_gears)
+rownames(distribution)[nrow(distribution)] <- "Blank 1"  
+
+
 
 #### Calculate catch and discards                     ####
 
@@ -80,6 +89,10 @@ landings <- landings_raw * 1e6 / 360                                        # Co
 catch <- landings / (1-discard_rate)                                        # Inflate landings with discards to total catch.
 
 catch[!is.finite(catch)] <- landings[!is.finite(catch)]                     # 0s and infinities mean no discard, so are overwritten with landings
+
+catch<-catch+bycatch
+
+
 
 #catch["Gillnets", "Cetacean"] <- catch["Gillnets", "Cetacean"] + (15.8 * 1e6 / 360 / domain_size) # Add extra discards following Mike's stories (see Notes)
 #catch["Gillnets", "Birds"] <- catch["Gillnets", "Birds"] +(2.015 * 1e6 / 360/ domain_size)        # Converting the units as for landings
@@ -94,14 +107,14 @@ all.equal(landings + discard_weight, catch)                                 # Qu
 
 #### Rearrange the distribution data                  ####
 
-new_distribution <- distribution[lookup$oldorder, hablookup$holdorder]
+new_distribution <- distribution[lookup$oldorder,hablookup$holdorder]
 colnames(new_distribution) <- hablookup$newhabs
 
 gear_hab <- data.frame(Gear_name = lookup$newgears,
                        Gear_code = lookup$gearcodes,
                        new_distribution)
 
-write.csv(gear_hab, str_glue("./StrathE2E/{implementation}/2010-2019-{ssp}/Param/fishing_distribution_{toupper(implementation)}_2010-2019.csv"),
+write.csv(gear_hab, str_glue("./StrathE2E/{implementation}/2010-2019/Param/fishing_distribution_{toupper(implementation)}_2010-2019.csv"),
           row.names=FALSE)
 
 #### Rearrange the landings, catch data, and discards ####
@@ -123,9 +136,6 @@ landings_new <- rearranged[[1]] ; catch_new <- rearranged[[2]] ; discards_new <-
 
 all.equal((landings_new + discards_new), catch_new)            # Check everything still balances
 
-landings_new$Demersal[7]
-discards_new$Demersal[7]
-catch_new$Demersal[7]
 
 #### Recalculate the discard_rate data                ####
 
@@ -141,7 +151,7 @@ discard_rate_final <- data.frame(Gear_name=lookup$newgears, Gear_code=lookup$gea
              "Discardrate_FDB","Discardrate_CSB","Discardrate_CZ","Discardrate_BD",
              "Discardrate_SL","Discardrate_CT","Discardrate_KP"))
 
-write.csv(discard_rate_final, str_glue("./StrathE2E/{implementation}/2010-2019-{ssp}/Param/fishing_discards_{toupper(implementation)}_2010-2019.csv"),
+write.csv(discard_rate_final, str_glue("./StrathE2E/{implementation}/2010-2019/Param/fishing_discards_{toupper(implementation)}_2010-2019.csv"),
           row.names=FALSE)
 
 #### Rearrange the effort (activity rate) data        ####
@@ -152,7 +162,7 @@ activity <- data.frame("Gear_name" = lookup$newgears,
                        "Plough_rate_(m2/s)"= lookup$abrasionrate)
 row.names(activity) <- NULL
 
-write.csv(activity,str_glue("./StrathE2E/{implementation}/2010-2019-{ssp}/Param/fishing_activity_{toupper(implementation)}_2010-2019.csv"), row.names=FALSE)
+write.csv(activity,str_glue("./StrathE2E/{implementation}/2010-2019/Param/fishing_activity_{toupper(implementation)}_2010-2019.csv"), row.names=FALSE)
 
 #### create the fishing power table                   ####
 
@@ -179,7 +189,7 @@ power <- mutate(power, Power_PF = Power_PF * mMNpergWW["PF"],  # Convert to nitr
                 Power_CT = Power_CT * mMNpergWW["CT"],
                 Power_KP = Power_KP * mMNpergWW["KP"])     
 
-write.csv(power, str_glue("./StrathE2E/{implementation}/2010-2019-{ssp}/Param/fishing_power_{toupper(implementation)}_2010-2019.csv"),
+write.csv(power, str_glue("./StrathE2E/{implementation}/2010-2019/Param/fishing_power_{toupper(implementation)}_2010-2019.csv"),
           row.names=FALSE)
 
 #### Target data                                      ####
@@ -193,7 +203,7 @@ discard_weight_target <- data.frame(Gear_name = lookup$newgears,
                                     Gear_code = lookup$gearcodes,
                                     discard_weight_target) 
 
-write.csv(discard_weight_target, str_glue("./StrathE2E/{implementation}/2010-2019-{ssp}/Target/TARGET_raw_discards_t_m2_y_{toupper(implementation)}_2010-2019.csv"),
+write.csv(discard_weight_target, str_glue("./StrathE2E/{implementation}/2010-2019/Target/TARGET_raw_discards_t_m2_y_{toupper(implementation)}_2010-2019.csv"),
           row.names = FALSE)
 
 #Now calculate the total ANNUAL landings, catch and discards of each guild gWW/m2/y
@@ -209,14 +219,14 @@ write.csv(discard_weight_target, str_glue("./StrathE2E/{implementation}/2010-201
 
 #### Reality check                                    ####
 
-BSarea <-domain_size                                             # Barents Sea total area in m2
-landings_tonnes <- (colSums(landings_new)) * 360 * BSarea / 1e6 # Does this match the data from ICES/FAO, Norway and STECF ???
+NSarea <-domain_size                                             # Norwegian Sea total area in m2
+landings_tonnes <- (colSums(landings_new)) * 360 * NSarea / 1e6 # Does this match the data from ICES/FAO, Norway and STECF ???
 
-test <- readRDS("./Data/Norwegian_sea_Tanguy2/Norwegian_sea/2010-2019/Object/International landings.rds") %>%     # Re-import international landings in tonnes
+test <- readRDS("./Data/Fishing_objects_01_25_TG/Data/Norwegian_sea_Tanguy2/Norwegian_sea/2010-2019/Object/International landings.rds") %>%     # Re-import international landings in tonnes
   colSums() %>%                                                 # Total over gears
   as.data.frame() %>% 
   rename("start_weight" = '.') %>%                              
-  mutate(start_weight = start_weight * BSarea) %>%              # Scale to Barents Sea
+  mutate(start_weight = start_weight * NSarea) %>%              # Scale to Barents Sea
   rownames_to_column("Guild") %>%                               
   full_join(landings_tonnes %>%                                 # Match to the new landings which have been converted to tonnes
               as.data.frame() %>%                               # Process as above to allow a join
